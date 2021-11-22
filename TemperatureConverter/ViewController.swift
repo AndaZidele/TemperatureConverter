d8ce11650497f2927c99c5c6c02963f4bd27e3d8
AndaZidele <87810405+AndaZidele@users.noreply.github.com>
2021-11-22 13:02:49 +0200

    withItems

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -9,10 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsiusL: UILabel!
+    @IBOutlet weak var tempS: UISlider!
+    @IBOutlet weak var convertedTempL: UILabel!
+    @IBOutlet weak var tempSegmentC: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        print("tempS:" ,tempS.value)
+        updateTempLForSlider(value: tempS.value)
+    }
+    
+    
+    @IBAction func segmentValueChanged(_ sender: Any) {
+        print("tempS:" ,tempS.value)
+        updateTempLForSlider(value: tempS.value)
+    }
+    
+    func updateTempLForSlider(value: Float){
+            let celsiusTemp = Int(value)
+        celsiusL.text = "\(celsiusTemp) ºC"
+        
+        var convertedTempString = ""
+        switch tempSegmentC.selectedSegmentIndex {
+        case 0:
+            let fahreinheitTempString = String(format: "%.2F", convertTempFrom(celsius: celsiusTemp).fahrenheit)
+            convertedTempString = fahreinheitTempString + " ºF"
+        default:
+            let kelvinTempString = String(format: "%.2F", convertTempFrom(celsius: celsiusTemp).kelvin)
+            convertedTempString = kelvinTempString + " K"
+        }
+        convertedTempL.text = convertedTempString
+    }
+    
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double){
+        let fahrenheit = (Double(celsius) * 9 / 5) + 32
+        let kelvin = Double(celsius) + 273.15
+        return (fahrenheit, kelvin)
+    }
+    
 
 
 }
